8023ed1cbcca8f28cc176f130f3c03f18b621d94
gfrraga <55815311+gfrraga@users.noreply.github.com>
2019-10-03 20:23:17 -0300

    Update DateFormat.java

--- src/main/java/Model/DateFormat.java
+++ src/main/java/Model/DateFormat.java
@@ -3,12 +3,16 @@ package Model;
 import java.text.SimpleDateFormat;
 
 public enum DateFormat {
+    //atribuindo um formato padrão para a data
     PADRAO("dd/MM/yyyy");
     private SimpleDateFormat dateFormat;
+    
+    //função setDateFormat atribuindo isoladamente o dateFormat
     private DateFormat(String dateFormat){
         this.dateFormat = new SimpleDateFormat(dateFormat);
     }
 
+    //função para retornar isoladamente o DateFormat
     public SimpleDateFormat getDateFormat() {
         return dateFormat;
     }
