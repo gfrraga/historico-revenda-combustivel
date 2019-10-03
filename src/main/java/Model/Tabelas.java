c34867747a1a7462194a95dbdde8301c9e6a62cb
gfrraga <55815311+gfrraga@users.noreply.github.com>
2019-10-03 19:37:44 -0300

    Update Tabelas.java

--- src/main/java/Model/Tabelas.java
+++ src/main/java/Model/Tabelas.java
@@ -4,13 +4,20 @@ public enum Tabelas {
     REGISTROS("registros"),
     ENDERECO("endereco"),
     PRODUTO("produto");
-
+    
+    //criação de objeto
     private final String nomeDaTabela;
 
+    /*
+    armazena o 'nome' na memoria da variavel 'nomeDaTabela'
+    Sugestão mudar o nome da função para setNomeDaTabela 
+    */
     Tabelas(String nome){
         nomeDaTabela = nome;
     }
 
+    
+    //A função devolve o conteúdo presente na variavel 'nomeDaTabela'
     public String getNomeDaTabela(){
         return nomeDaTabela;
     }
